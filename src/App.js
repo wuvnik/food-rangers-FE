78eac42de4ebac0f74437b00fb1176e790fab0dd
nik <13nikshahmi@gmail.com>
2022-10-15 12:23:51 +0800

    add list food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
 import logo from './logo.svg';
 import './App.css';
+import User from "./pages/User";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <User />} />
+        <Route path="/landing" element={ <Landing />} />
+        <Route path="/provider" element={ <div></div>} />
+      </Routes>
+    </Router>
   );
 }
 
